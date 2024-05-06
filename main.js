22e988e5f0baedd6bdea9955b7206e25eb80e261
joaovitormr <128191067+joaovitormr@users.noreply.github.com>
2024-05-06 19:06:41 -0300

    continuação

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-30T23:59:59");
-const tempoObjetivo2 = new Date("2024-12-30T23:59:58");
-const tempoObjetivo3 = new Date("2024-12-30T23:59:58");
+const tempoObjetivo2 = new Date("2024-12-30T23:59:59");
+const tempoObjetivo3 = new Date("2024-12-30T23:59:59");
 const tempoObjetivo4 = new Date("2024-12-30T23:59:59");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
